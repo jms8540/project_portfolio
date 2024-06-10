82764de048f5b6687cb182c2c9908b219725ca87
jeonminsu <73738968+jms8540@users.noreply.github.com>
2024-06-10 14:43:34 +0900

    test4
    <할꺼>
    반응형
    내용
    gsap오류 수정

--- js/script.js
+++ js/script.js
@@ -1,6 +1,4 @@
-console.log("hihi");
-
-// scrollTrigger
+// ScrollTrigger for navigation links
 document.querySelectorAll('nav ul li a').forEach(anchor => {
     anchor.addEventListener('click', function (e) {
         e.preventDefault();
@@ -17,85 +15,44 @@ document.querySelectorAll('nav ul li a').forEach(anchor => {
     });
 });
 
+// Home section animations
+gsap.to('.arrow', { y: 0, repeat: -1, repeatDelay: 0.1 });
+gsap.to('.arrow', { y: 30, repeat: -1, yoyo: true });
 
-// home
-gsap.to('.arrow', { y: 0, repeat: -1, repeatDelay: 0.1, })
-gsap.to('.arrow', { y: 30, repeat: -1, yoyo: true, })
-
-
-//about
-let about = gsap.fromTo('.innerItem > div', { opacity: 0, duration: 0.2, }, { opacity: 1, stagger: 0.2, });
+// About section animations
+let about = gsap.fromTo('.innerItem > div', { opacity: 0, duration: 0.2 }, { opacity: 1, stagger: 0.2 });
 ScrollTrigger.create({
     trigger: '.skillBox',
-    // markers: true,
     start: "top center",
     end: "bottom center",
     animation: about,
     toggleActions: "restart none none reverse",
 });
 
-
-// project
-// let projects = gsap.fromTo('.innerItem > div', { opacity: 0, duration: 0.2, }, { opacity: 1, stagger: 0.2, });
-// ScrollTrigger.create({
-//     trigger: '.skillBox',
-//     markers: true,
-//     start: "top center",
-//     end: "bottom center",
-//     animation: projects,
-//     toggleActions: "restart none none reverse",
-// });
-
-// const sectionColors = [
-//     "#f2eee5",
-//     "#e5c1c5",
-//     "#c3e2dd",
-//     "#6eceda",
-//     "#FB9DA7",
-//     "#FCCCD4",
-//     "#FBDEA2",
-//     "#F2E2C6",
-//     "#8EB695",
-// ];
-
-// gsap.set(".section", {
-//     backgroundColor: gsap.utils.wrap(sectionColors),
-// });
-
-// const textBox = document.querySelectorAll('.textContainer')[0].offsetWidth;
-// gsap.to('.textContainer', {
-//     x: -(textBox - window.innerWidth),
-//     scrollTrigger: {
-//         trigger: ".demo-text",
-//         start: "20% center",
-//         end: "80% center",
-//         markers: true,
-//         scrub: 1,
-//         onScrubComplete() {
-//             console.log("Scrub running");
-//         },
-//     },
-// });
-
-// gsap.from('.imageContainer',
-//     {
-//         x: -(document.querySelector('.imageContainer').offsetWidth - innerWidth),
-//         scrollTrigger: {
-//             trigger: ".demo-image",
-//             start: "26% center",
-//             end: "74% center",
-//             markers: true,
-//             scrub: 3,
-//         },
-//     });
+// Project section animations
+let cont = document.querySelector("#projects");
+gsap.to(".project_list", {
+  ease: "none",
+  x: () => -(cont.scrollWidth - window.innerWidth),
+  scrollTrigger: {
+    trigger: cont,
+    pin: true, 
+    start: "center center", 
+    end: () => "+=" + (cont.scrollWidth - window.innerWidth), 
+    scrub: true,
+    invalidateOnRefresh: true,
+    onLeave: () => {
+        ScrollTrigger.refresh();
+    },
+    // markers: true,
+  }
+});
 
 
-// blog&contact
-
+// Blog & Contact section animations
 const lists = document.querySelectorAll(".Contact_ul li");
 lists.forEach((e, index) => {
-    gsap.set('.Contact_ul li', { y: 80, opacity: 0, });
-
+    gsap.set('.Contact_ul li', { y: 80, opacity: 0 });
     gsap.to(e, {
         y: 0,
         opacity: 1,
@@ -103,48 +60,7 @@ lists.forEach((e, index) => {
             trigger: e,
             start: "bottom 100%",
             end: "bottom 100%",
-            // markers: true,
             scrub: 1.4,
         },
     });
 });
-
-
-// innerHeight
-// window.addEventListener('resize', () => {
-//     document.body.style.height = `${window.innerHeight}px`;
-// });
-
-// document.addEventListener('DOMContentLoaded', () => {
-//     gsap.registerPlugin(ScrollTrigger);
-
-//     // Update body height initially
-//     document.body.style.height = `${window.innerHeight}px`;
-
-//     gsap.utils.toArray('.navbar a').forEach(anchor => {
-//         anchor.addEventListener('click', function (e) {
-//             e.preventDefault();
-//             gsap.to(window, {
-//                 scrollTo: { y: `#${this.dataset.target}`, autoKill: false },
-//                 duration: 1
-//             });
-//         });
-//     });
-
-//     gsap.from(".homeText", { opacity: 0, y: -50, duration: 1, stagger: 0.2 });
-
-//     ScrollTrigger.batch(".skill_icon", {
-//         start: "top 80%",
-//         onEnter: batch => gsap.to(batch, { opacity: 1, y: 0, stagger: 0.15 })
-//     });
-
-//     ScrollTrigger.batch(".projectItem", {
-//         start: "top 80%",
-//         onEnter: batch => gsap.to(batch, { opacity: 1, y: 0, stagger: 0.15 })
-//     });
-
-//     ScrollTrigger.batch(".Contact_ul li", {
-//         start: "top 80%",
-//         onEnter: batch => gsap.to(batch, { opacity: 1, y: 0, stagger: 0.15 })
-//     });
-// });
